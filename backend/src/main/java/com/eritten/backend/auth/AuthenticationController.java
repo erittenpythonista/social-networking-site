81d85154274e81d9e8db9720e4c63536e8f102d8
erittenpythonista <eritten.gyau@amalitech.com>
2024-02-28 21:21:14 -1100

    add mailservice class to send verification code and welcome messages to users

--- backend/src/main/java/com/eritten/backend/auth/AuthenticationController.java
+++ backend/src/main/java/com/eritten/backend/auth/AuthenticationController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/users")

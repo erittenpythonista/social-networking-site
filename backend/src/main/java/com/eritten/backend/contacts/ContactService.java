c5795a55af3e9700b689ed5616c6d924ced75a50
erittenpythonista <eritten.gyau@amalitech.com>
2024-03-04 04:31:11 -1100

    feat/contacts_management: define getcontacts method in the contact service class to return list of contacts associated with a given user

--- backend/src/main/java/com/eritten/backend/contacts/ContactService.java
+++ backend/src/main/java/com/eritten/backend/contacts/ContactService.java
@@ -1,21 +1,26 @@
 package com.eritten.backend.contacts;
 
-import org.hibernate.mapping.List;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.eritten.backend.models.Contact;
+import com.eritten.backend.models.User;
 import com.eritten.backend.repositories.UserRepository;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
 @AllArgsConstructor
 @Service
 public class ContactService {
     private final UserRepository userRepository;
 
-    public List<Contact> getContacts() {
+    public List<Contact> getContactsByEmail(String email) {
+        // Find the user by email
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
 
+        // Return the contacts associated with the user
+        return user.getContacts();
     }
-
 }

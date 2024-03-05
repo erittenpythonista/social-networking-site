479bd9ed665edc862486b71828d0d6e99359ccf9
erittenpythonista <eritten.gyau@amalitech.com>
2024-03-05 04:10:03 -1100

    feat/text_chat: Add websocket dependencies

---
+++ backend/src/main/java/com/eritten/backend/config/WebsocketConfig.java
@@ -0,0 +1,9 @@
+package com.eritten.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebsocketMessageBroker
+public class WebsocketConfig {
+
+}
